ad62845c4a37fc083a33fe9ecefb3f5ae4465a01
eduardo <mejiasavilaeduardo@gmail.com>
2010-05-06 01:35:20 -0430

    pruebas en clase 4

--- clase4/sql5.sql
+++ clase4/sql5.sql
@@ -1,5 +1,5 @@
-select
-	pais.nombre
+select 
+	pais.nombre,
 	(select count(*) from ciudad where ciudad.idpais = pais.id) as "total ciudades",
 	(select sum(ciudad.habitantes) from ciudad where ciudad.idpais = pais.id) as "total habitantes"
 	from pais
